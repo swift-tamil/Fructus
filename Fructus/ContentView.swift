126c4969e81602b2d3530175535b3c40a8c370e5
MohamedGoush <0x1110111@gmail.com>
2023-11-04 01:46:24 +0530

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by python on 04/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by python on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
